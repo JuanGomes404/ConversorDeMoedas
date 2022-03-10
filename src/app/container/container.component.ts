1f0d56465f871e697b726e5560bd75f143fdedb1
Juan Gomes <juangsantanna@gmail.com>
2022-03-10 13:25:52 -0300

    listagem das moedas integrada com a API completa

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,5 +1,3 @@
-import { HistoricoComponent } from './../historico/historico.component';
-import { Historico } from './../historico/historico';
 import { ContainerResultadoService } from 'src/app/service/container-resultado.service';
 import { MoedasService } from './../service/moedas.service';
 import { Conversao } from './conversao';
@@ -7,7 +5,6 @@ import { Conversao } from './conversao';
 import { Component, Input, OnInit } from '@angular/core';
 import { ConversorService } from '../service/conversor.service';
 import { Router } from '@angular/router';
-import { makeBindingParser } from '@angular/compiler';
 
 @Component({
   selector: 'app-container-conversor',
@@ -15,37 +12,42 @@ import { makeBindingParser } from '@angular/compiler';
   styleUrls: ['./container.component.css'],
 })
 export class ContainerComponent implements OnInit {
-  @Input() listaMoedas = this.moedasService.getListaMoedas();
+
   id: number;
   constructor(
     private service: ConversorService,
     public cvs: Conversao,
     private router: Router,
     private moedasService: MoedasService,
-    private cntResultadoService: ContainerResultadoService,
-    private historico:HistoricoComponent
-  ) {}
-
+    private cntResultadoService: ContainerResultadoService
+  ) {
+    this.moedasService.connectAPI();
+  }
+  @Input() listaMoedas = this.moedasService.getListaMoedas();
   ngOnInit() {
     this.cvs.valor = 0;
+
+
   }
 
-  generateId(){
-   let id = Math.random()*10;
+  generateId() {
+    let id = Math.random() * 10;
 
-   return id;
+    return id;
   }
   converter() {
-    if(this.cvs.valor == 0 || this.cvs.valor === null || this.cvs.valor === undefined){
-      alert("Insira os dados corretamente");
-    }
-    else{
+    if (
+      this.cvs.valor == 0 ||
+      this.cvs.valor === null ||
+      this.cvs.valor === undefined
+    ) {
+      alert('Insira os dados corretamente');
+    } else {
       this.service.converteMoeda(this.cvs).subscribe((result: any) => {
         //console.log('API RODANDO! :)');
         //console.log(result)
 
         this.cntResultadoService.organizarResultado(result);
-
 
         let dadosConversao = {
           id: this.generateId(),
@@ -63,12 +65,11 @@ export class ContainerComponent implements OnInit {
       });
     }
   }
-  }
+}
 
-  /*teste(){
+/*teste(){
       console.log(this.valor);
       console.log(this.selectedOptionMoedaAtual);
       console.log(this.selectedOptionMoedaConverter);
    }
  */
-
--- src/app/service/moedas.service.ts
+++ src/app/service/moedas.service.ts
@@ -1,40 +1,29 @@
-export class MoedasService{
-  private listaMoedas = [
-    { symbols: 'AUD', descricao: 'Dólar australiano' },
-    { symbols: 'BGN', descricao: 'Lev búlgaro' },
-    { symbols: 'BRL', descricao: 'Real brasileiro' },
-    { symbols: 'CAD', descricao: 'Dólar canadense' },
-    { symbols: 'CHF', descricao: 'Franco suíço' },
-    { symbols: 'CNY', descricao: 'Yuan Chinês' },
-    { symbols: 'CZK', descricao: 'Coroa República Tcheca' },
-    { symbols: 'DKK', descricao: 'Coroa dinamarquesa' },
-    { symbols: 'EUR', descricao: 'Euro' },
-    { symbols: 'GBP', descricao: 'Libra Esterlina' },
-    { symbols: 'HKD', descricao: 'Dólar de Hong Kong' },
-    { symbols: 'HRK', descricao: 'Coroa Croácia' },
-    { symbols: 'HUF', descricao: 'Florim húngaro' },
-    { symbols: 'IDR', descricao: 'Rupia indonésia' },
-    { symbols: 'ILS', descricao: 'Novo shekel israelense' },
-    { symbols: 'INR', descricao: 'Rupia indiana' },
-    { symbols: 'JPY', descricao: 'Iene japonês' },
-    { symbols: 'KRW', descricao: 'Won sul-coreano' },
-    { symbols: 'MXN', descricao: 'Peso mexicano' },
-    { symbols: 'MYR', descricao: 'Malásia Ringgit' },
-    { symbols: 'NOK', descricao: 'Coroa Noruega' },
-    { symbols: 'NZD', descricao: 'Dólar da Nova Zelândia' },
-    { symbols: 'PHP', descricao: 'Peso filipino' },
-    { symbols: 'PLN', descricao: 'Złoty Polónia' },
-    { symbols: 'RON', descricao: 'Leu romeno' },
-    { symbols: 'RUB', descricao: 'Belarus Ruble' },
-    { symbols: 'SEK', descricao: 'Coroa Suécia' },
-    { symbols: 'SGD', descricao: 'Dólar de Singapura' },
-    { symbols: 'THB', descricao: 'Baht Tailândia' },
-    { symbols: 'TRY', descricao: 'Lira turca' },
-    { symbols: 'USD', descricao: 'Dólar dos Estados Unidos' },
-    { symbols: 'ZAR', descricao: 'Rand África do Sul' },
-  ];
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+@Injectable()
+export class MoedasService {
+  private listaMoedas = [];
+  public connectAPI(){
+    this.connectMoedasAPI();
+  }
+  constructor(private http: HttpClient) {}
+  private connectMoedasAPI() {
+    let requestURL = 'https://api.exchangerate.host/symbols';
 
-  getListaMoedas(){
+    this.http.get<Object>(requestURL).subscribe((res) =>{
+       //this.listaMoedas.push(res);
+       console.log(res)
+       this.organizarResultado(res);
+    })
+
+  }
+  organizarResultado(res){
+    let obj = res.symbols;
+    Object.keys(obj).forEach(key => {
+      this.listaMoedas.push(obj[key]);
+    })
+  }
+  public getListaMoedas() {
     return this.listaMoedas;
   }
 }
